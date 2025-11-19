8b6b6c6b11339bcf22b5ec1638925b72fbd8cca2
David <davidagdaw@gmail.com>
2025-11-19 16:46:03 +0000

    Creación de ProductoJpaRepositoryImpl y ProductoEntityJpaRepository, siguiente paso cambiar el mapper

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/repository/ProductoEntityJpaRepository.java
@@ -0,0 +1,7 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.repository;
+
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.ProductoRepository;
+
+public interface ProductoEntityJpaRepository extends{
+
+}
