7278ad3a1029ed95a7dd835d3d3c2e8adbb776eb
David <davidagdaw@gmail.com>
2025-11-26 18:54:47 +0000

    Arreglando errores

--- src/test/java/es/etg/daw/dawes/java/rest/restfull/tests/CategoriaEntityJpaRepositoryTest.java
+++ src/test/java/es/etg/daw/dawes/java/rest/restfull/tests/CategoriaEntityJpaRepositoryTest.java
@@ -1,4 +1,4 @@
-package es.etg.daw.dawes.java.rest.restfull;
+package es.etg.daw.dawes.java.rest.restfull.tests;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
