ddaf3c3da7ebd45a72c1f7ab42d9b81d0ac054b0
DavidAG <david@gmail.com>
2026-01-15 16:49:45 +0000

    Implementacion Security Config

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/domain/UsuarioId.java
@@ -0,0 +1,12 @@
+package es.etg.daw.dawes.java.rest.restfull.auth.domain;
+
+import es.etg.daw.dawes.java.rest.restfull.common.domain.model.Identificador;
+
+public class UsuarioId extends Identificador{
+
+    public UsuarioId(Integer value) {
+        super(value);
+    }
+    
+}
+
