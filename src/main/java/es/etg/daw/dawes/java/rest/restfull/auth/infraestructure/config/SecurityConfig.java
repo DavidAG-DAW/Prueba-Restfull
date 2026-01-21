f3e7c2140ed666c158b093b01bdf7e3aa22ced80
DavidAG <david@gmail.com>
2026-01-21 15:21:02 +0000

    Backup para pasarlo a linux

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.db.jpa.entity.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.mapper.UserMapper;
